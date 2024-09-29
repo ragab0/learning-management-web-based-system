d1b2af2205e73d9a148ed364c817840270f8c1b7
ragab0 <legendragab@gmail.com>
2024-09-29 16:20:59 +0300

    Merge abas's tasks into the new structure

--- src/apps/mainApp/pages/ShoppingCart/Components/Details/Details.js
+++ src/apps/mainApp/pages/ShoppingCart/Components/Details/Details.js
@@ -8,6 +8,34 @@ export default function Details() {
       <Link to={"checkout"} style={{ fontSize: "25px", color: "blue" }}>
         Proceed to checkout
       </Link>
+      <h4 className=" text-dark">Order Details</h4>
+      <div className="orderDetails p-3 text-black mt-4 mb-4">
+        <div className="orderPrice d-flex justify-content-between my-2">
+          <span>Price</span>
+          <span className="num">$300.00</span>
+        </div>
+        <div className="orderPrice d-flex justify-content-between my-2">
+          <span>Discount</span>
+          <span className="num">-$10.00</span>
+        </div>
+        <div className="orderPrice d-flex justify-content-between my-2">
+          <span>Tax</span>
+          <span className="num">$20.00</span>
+        </div>
+        <div className="orderPrice d-flex justify-content-between my-3">
+          <span className=" fw-bolder fs-5">Total</span>
+          <span className="num fs-5">$290.00</span>
+        </div>
+      </div>
+      <Link
+        className=" text-center"
+        to={"checkout"}
+        style={{ fontSize: "25px", color: "blue" }}
+      >
+        <button className=" w-100 m-auto d-block btn px-5 rounded-3">
+          Proceed to Checkout
+        </button>
+      </Link>
     </div>
   );
 }
