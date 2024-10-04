f8d890dfd5af680383dff924cfeee231a83b4ae2
Abbass khalid <hm528366@gmail.com>
2024-10-04 09:17:21 +0300

    Rebasing piso-sprint3-tasks

--- src/apps/dashboardApp/pages/Course/components/ChaptersTab/ChaptersTab.js
+++ src/apps/dashboardApp/pages/Course/components/ChaptersTab/ChaptersTab.js
@@ -1,20 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ChaptersTab.css";
 import { NavLink, Outlet } from "react-router-dom";
 
 export default function ChaptersTab() {
+  const [activeTab, setActiveTab] = useState("details");
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
   return (
     <div className="chapters-tab">
       <header className="chapters-tab-header">
-        <h2>Data structures & Algorithms</h2>
+        <div className="chapterNav">
+          <h3 className=" text-capitalize"> chapter1-the solid state</h3>
+          <div>
+            <button className="btn btn-danger" type="button">
+              Delete
+            </button>
+            <button className="btn btn-light mx-4" type="button">
+              Move To Draft
+            </button>
+            <button className="btn btn-primary" type="button">
+              Add Course
+            </button>
+          </div>
+        </div>
         <ul className="chapters-tab-tabs">
-          <li>
-            <NavLink to="" end>
+          <li className=" details text-primary">
+            <NavLink
+              style={{
+                color: activeTab === "details" ? "#3b82f6" : "#475569",
+                borderBottom:
+                  activeTab === "details" ? "3px solid #3b82f6" : "none",
+              }}
+              onClick={() => handleTabClick("details")}
+              to=""
+              end
+            >
               Details
             </NavLink>
           </li>
           <li>
-            <NavLink to="resources">Resources</NavLink>
+            <NavLink
+              style={{
+                color: activeTab === "resources" ? "#3b82f6" : "#475569",
+                borderBottom:
+                  activeTab === "resources" ? "3px solid #3b82f6" : "none",
+              }}
+              onClick={() => handleTabClick("resources")}
+              to="resources"
+            >
+              Resources
+            </NavLink>
           </li>
         </ul>
       </header>
--- src/apps/dashboardApp/pages/Course/components/ChaptersTab/ChaptersTabResourcesTab/ChaptersTabResourcesTab.js
+++ src/apps/dashboardApp/pages/Course/components/ChaptersTab/ChaptersTabResourcesTab/ChaptersTabResourcesTab.js
@@ -2,7 +2,51 @@ import React from "react";
 import "./ChaptersTabResourcesTab.css";
 
 export default function ChaptersTabResourcesTab() {
+  const renderSection = (title, content) => (
+    <div className="section">
+      <h6>{title}</h6>
+      <h5 className="text-black fs-6 fw-normal">{content}</h5>
+    </div>
+  );
+
+  const renderUploadSection = (label, fileTypes) => (
+    <div className="upload-section text-center my-5">
+      <i className="fa-solid fa-upload"></i>
+      <h3 className="fs-4 my-4">
+        Drag and drop files, or <span className="text-primary">Browse</span>
+      </h3>
+      <h6 className="fw-light">Upload files in {fileTypes}.</h6>
+    </div>
+  );
+
   return (
-    <div className="chapters-tab-resources-tab">ChaptersTabResourcesTab</div>
+    <div className="chapters-tab-resources-tab">
+      <h3 className="fs-5 fw-bold mt-3">Upload Notes</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+      <div className="chapters-tab-resources-body">
+        {renderSection("Content Type", "Notes")}
+        {renderSection("Title", "Notes")}
+        <div className="description section">
+          <h6>Description</h6>
+          <h5 className="fs-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </h5>
+        </div>
+      </div>
+      <div className="upload">
+        <h6 className="mt-5">Upload File</h6>
+        {renderUploadSection("Upload File", "Text, Word, PDF")}
+        <h6 className="mt-5">Upload Thumbnail</h6>
+        {renderUploadSection("Upload Thumbnail", "JPEG, PNG")}
+      </div>
+    </div>
   );
 }
