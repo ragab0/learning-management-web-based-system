ce2bce55299a31590b3c41e2295ee48faa24ac7a
ragab0 <legendragab@gmail.com>
2024-10-15 00:12:22 +0300

    Add the API URL of production

--- src/utils/myAxios.js
+++ src/utils/myAxios.js
@@ -4,7 +4,9 @@ const REACT_APP_ENV = process.env.REACT_APP_ENV;
 
 const myAxios = axios.create({
   baseURL:
-    REACT_APP_ENV === "development" ? "http://localhost:3500/api/v1" : "",
+    REACT_APP_ENV === "development"
+      ? "http://localhost:3500/api/v1"
+      : "https://lms-depi-d574ce496085.herokuapp.com/api/v1",
   withCredentials: true,
   // timeout: 8000,
   headers: {
